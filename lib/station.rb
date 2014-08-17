4952ed3f5b152a628cf70f2386ecf739a8858a7a
Justin Pacubas <jpacubas@gmail.com>
2014-08-16 18:33:52 -0700

    Pass second test for station class for saving a new station to the database.

--- lib/station.rb
+++ lib/station.rb
@@ -2,7 +2,26 @@ class Station
 	attr_reader :id, :name
 
 	def initialize(attributes)
-		@id = attributes['id']
+		@id = attributes['id'].to_i
 		@name = attributes['name']
+	end
+
+	def == another_station
+		self.id == another_station.id && self.name == another_station.name
+	end
+
+	def save
+		results = DB.exec("INSERT INTO stations (name) VALUES ('#{@name}') RETURNING id;")
+		@id = results.first['id'].to_i
+	end
+
+	def self.all
+		results = DB.exec("SELECT * FROM stations;")
+		stations = []
+		results.each do |result|
+			new_station = Station.new(result)
+			stations << new_station
+		end
+		stations
 	end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -5,4 +5,10 @@ describe Station do
 		test_station = Station.new({'name' => 'Portland'})
 		expect(test_station).to be_an_instance_of Station
 	end
+
+	it 'saves a new station to the database' do
+		test_station = Station.new({'name' => 'Portland'})
+		test_station.save
+		expect(Station.all).to eq [test_station]
+	end
 end
