91a0951e52d840e593238ab32ec15cfb91bf057f
Justin Pacubas <jpacubas@gmail.com>
2014-08-16 19:10:51 -0700

    Add functionality to associate a train line with a station and return all train lines associated with a station.

--- lib/train_line.rb
+++ lib/train_line.rb
@@ -24,4 +24,19 @@ class Train_line
 		end
 		train_lines
 	end
+
+	def self.stop(train_line_id, station_id)
+		results = DB.exec("INSERT INTO stops (train_line_id, station_id) VALUES (#{train_line_id}, #{station_id}) RETURNING id;")
+		stop_id = results.first['id'].to_i
+	end
+
+	def self.station_train_line_list(station_id)
+		results = DB.exec("SELECT train_lines.* FROM stations JOIN stops ON (stations.id = stops.station_id) JOIN train_lines ON (stops.train_line_id = train_lines.id) WHERE stations.id = #{station_id};")
+		train_lines = []
+		results.each do |result|
+			new_train_line = Train_line.new(result)
+			train_lines << new_train_line
+		end
+		train_lines
+	end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -11,4 +11,18 @@ describe Station do
 		test_station.save
 		expect(Station.all).to eq [test_station]
 	end
+
+	it 'associates a train line with a station and returns all train lines associated with a station' do
+		test_train_line = Train_line.new({'name' => 'Grasshopper'})
+		test_train_line.save
+		test_train_line1 = Train_line.new({'name' => 'The Norris'})
+		test_train_line1.save
+		test_station = Station.new({'name' => 'Portland'})
+		test_station.save
+		Train_line.stop(test_train_line.id, test_station.id)
+		binding.pry
+		Train_line.stop(test_train_line1.id, test_station.id)
+		train_lines = Train_line.station_train_line_list(test_station.id)
+		expect(train_lines).to eq [test_train_line, test_train_line1]
+	end
 end
